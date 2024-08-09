0d5bf8476b604fa9507b2c1fcf14311f31678914
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-08-09 19:57:36 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         violet: "hsl(257, 40%, 49%)",
         "soft-magenta": "hsl(300, 69%, 71%)",
       },
+      boxShadow: {
+        button: "1px 3px 5px 0px rgba(0, 0, 0, 0.27)",
+      },
     },
   },
   plugins: [],
